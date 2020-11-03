885ae4ee1a1430607089dbcafa8d1d396959869b
Andrei Krivošei <andrei.krivosei@ttu.ee>
2020-11-03 13:23:08 +0000

    Working algorithm. Debug info is commented.

--- a_star.py
+++ a_star.py
@@ -4,7 +4,7 @@ from heapq import heappush as push, heappop as pop
 def dist_yx(p1_yx, p2_yx):
     p1 = np.array(p1_yx)
     p2 = np.array(p2_yx)
-    return np.linalg.norm(p2 - p1, ord=2)
+    return np.linalg.norm(p2 - p1, ord=1)
 
 def sum_yx(p1_yx, p2_yx):
     return (p1_yx[0] + p2_yx[0], p1_yx[1] + p2_yx[1])
@@ -43,7 +43,7 @@ def path(cameFrom, curr):
 
 def search(grid, start_yx, goal_yx):
     hp = []
-    closed = []
+    visited = []
     cameFrom = {}
     ncols = len(grid[0]) - 1
     nrows = len(grid) - 1
@@ -51,50 +51,48 @@ def search(grid, start_yx, goal_yx):
 
 
     h = dist_yx(start_yx, goal_yx)
-    print(h)
+    # print(h)
     g = {start_yx: 0}
     f = {start_yx: h}
 
     push(hp, (f.get(start_yx, g_inf), start_yx))
     while hp:
-        print(f'In heap {len(hp)} item(s)')
+        # print(f'In heap {len(hp)} item(s)')
         curr = pop(hp)[-1]
+        # print(hp)
         
         if curr == goal_yx:
-            print('*'*100)
+            # print('*'*50)
+            # print('Solution:')
             p = path(cameFrom, curr)[::-1]
-            print(p)
+            # print(f'The shortest path:\n{p}')
             return p
-        
-        if curr in closed:
-            continue
-        
-        closed.append(curr)
         nbrs = neighbors(curr, (0, 0), (nrows, ncols))
 
-        print()
-        print('Neighbors:')
-        print('-'*40)
+        # print()
+        # print('Neighbors:')
+        # print('-'*40)
         for nbr in nbrs:
             new_g = g.get(curr, g_inf) + dist_yx(curr, nbr) * grid[nbr[0]][nbr[1]] * 10
-            print(f'g.get(curr, g_inf) = {g.get(curr, g_inf)}')
-            print(f'dist_yx(curr, nbr) = {dist_yx(curr, nbr) * grid[nbr[0]][nbr[1]]}')
-            print(f'New G {curr}-{nbr} = {new_g}')
+            # print(f'g.get(curr, g_inf) = {g.get(curr, g_inf)}')
+            # print(f'dist_yx(curr, nbr) = {dist_yx(curr, nbr) * grid[nbr[0]][nbr[1]]}')
+            # print(f'New G {curr}-{nbr} = {new_g}')
             if new_g < g.get(nbr, g_inf):
                 cameFrom[nbr] = curr
                 g[nbr] = new_g
-                f[nbr] = g.get(nbr, g_inf) + dist_yx(goal_yx, nbr)
-                print(f'==> dist_yx(goal_yx, nbr) = {dist_yx(goal_yx, nbr)}')
-                push(hp, (f.get(nbr, g_inf), nbr))
+                f[nbr] = (g.get(nbr, g_inf) + dist_yx(goal_yx, nbr))
+                # print(f'==> dist_yx(goal_yx, nbr) = {dist_yx(goal_yx, nbr)}')
+                if nbr not in hp:
+                    push(hp, (f.get(nbr, g_inf), nbr))
 
 
-        print(nbrs)
+        # print(nbrs)
 
-        print()
-        print(f'Closed: {closed}')
-        print(f'g = {g}')
-        print(f'f = {f}')
-        print(f'cameFrom = {cameFrom}')
+        # print()
+        # print(f'Visited: {len(set(visited))}')
+        # print(f'g = {g}')
+        # print(f'f = {f}')
+        # print(f'cameFrom = {cameFrom}')
 
 if __name__ == '__main__':
 
@@ -108,8 +106,6 @@ if __name__ == '__main__':
             ]
 
     goal = (len(grid)-1, len(grid[0])-1)
-    search(grid,
-           (0, 0),
-        #    (goal[0]-2, goal[1]-2),
-           (goal[0], goal[1]),
-           )
+    answer = search(grid, (0, 0), (goal[0], goal[1]))
+    print(answer)
+    assert answer == [(0, 0), (1, 0), (2, 0), (3, 0), (4, 1), (4, 2), (3, 3), (2, 4), (3, 5)]
